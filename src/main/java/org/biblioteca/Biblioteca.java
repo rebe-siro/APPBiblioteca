d217e516d91d1cc15e9f1e2617e68a84369e6594
CtrlLuzzio <130322396+CtrlLuzzio@users.noreply.github.com>
2024-12-05 23:35:04 -0400

    Add files via upload

--- src/main/java/org/biblioteca/Biblioteca.java
+++ src/main/java/org/biblioteca/Biblioteca.java
@@ -5,6 +5,7 @@ package org.biblioteca;
 import org.biblioteca.controller.LoginScreenController;
 import org.biblioteca.view.LoginScreen;
 import org.biblioteca.model.Conexion;
+import org.biblioteca.model.UserConsults;
 
 /**
  *
@@ -14,13 +15,15 @@ public class Biblioteca {
 
     public static void main(String[] args) { 
         
-        //Conexion cn = new Conexion();
-        //cn.getConexion();
+        Conexion cn = new Conexion();
+        cn.getConexion();
+        
+        UserConsults uc = new UserConsults();
         
         LoginScreen ls = new LoginScreen();
         ls.setVisible(true);
         ls.setLocationRelativeTo(null);
         
-        LoginScreenController lsc = new LoginScreenController(ls);
+        LoginScreenController lsc = new LoginScreenController(ls, uc);
     }
 }
