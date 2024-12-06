9b619e549c45983d89b821c5f757acf64cde24a4
CtrlLuzzio <130322396+CtrlLuzzio@users.noreply.github.com>
2024-12-06 15:40:28 -0400

    Add files via upload

--- src/main/java/org/biblioteca/controller/LoginScreenController.java
+++ src/main/java/org/biblioteca/controller/LoginScreenController.java
@@ -1,48 +1,59 @@
-//Este es el controlador de la vista
 package org.biblioteca.controller;
 
 import org.biblioteca.view.LoginScreen;
 import org.biblioteca.view.MainPage;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JOptionPane;
+import org.biblioteca.view.BasicMainPage;
+import org.biblioteca.view.AdminDecorator;
+import org.biblioteca.view.PSDecorator;
 import org.biblioteca.model.UserConsults;
+
+import javax.swing.JOptionPane;
+import org.biblioteca.view.ErrorDecorator;
 
 public class LoginScreenController {
     private final LoginScreen ls;
     private final UserConsults uc;
-    
-    
+
     public LoginScreenController(LoginScreen ls, UserConsults uc) {
         this.ls = ls;
         this.uc = uc;
 
-        this.ls.getJButtonLogin().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String enteredUser = ls.getJTextFieldUser().getText();
-                String enteredPassword = new String(ls.getJPasswordField().getPassword());
-                if (uc.validateLogin(enteredUser, enteredPassword)){
-                    ls.dispose();
-                    MainPage mp = new MainPage();
-                    mp.setVisible(true);
-                    mp.setLocationRelativeTo(null);
-                }
-                else {
-                    JOptionPane.showMessageDialog(ls, "Nombre de usuario o contraseña incorrectos", "Error de autenticación", JOptionPane.WARNING_MESSAGE);
-                }
+        this.ls.getJButtonLogin().addActionListener(e -> {
+            String enteredUser = ls.getJTextFieldUser().getText();
+            String enteredPassword = new String(ls.getJPasswordField().getPassword());
+            if (uc.validateLogin(enteredUser, enteredPassword)) {
+                int userType = uc.getUserType(enteredUser);
+                ls.dispose();
+                MainPage mp = createMainPage(userType);
+                mp.enableButtons();
+                mp.display();
+                System.out.println(userType);
+            } else {
+                JOptionPane.showMessageDialog(ls, "Nombre de usuario o contraseña incorrectos", "Error de autenticación", JOptionPane.WARNING_MESSAGE);
             }
         });
-        this.ls.getJButtonGuest().addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                    ls.dispose();
-                    JOptionPane.showMessageDialog(ls, "Has entrado como invitado", "", JOptionPane.INFORMATION_MESSAGE);
-                    MainPage mp = new MainPage();
-                    mp.setVisible(true);
-                    mp.setLocationRelativeTo(null);
-            }
+
+        this.ls.getJButtonGuest().addActionListener(e -> {
+            ls.dispose();
+            JOptionPane.showMessageDialog(ls, "Has entrado como invitado", "", JOptionPane.INFORMATION_MESSAGE);
+            MainPage mp = new BasicMainPage();
+            mp.display();
         });
     }
-}
 
+    private MainPage createMainPage(int userType) {
+        MainPage basicMainPage = new BasicMainPage();
+        switch (userType) {
+            case 0:
+                return new PSDecorator(basicMainPage);
+            case 1:
+                return new PSDecorator(basicMainPage);
+            case 2:
+                return new AdminDecorator(basicMainPage);
+            case -1:
+                return new ErrorDecorator(basicMainPage);
+            default:
+                return basicMainPage;
+        }
+    }
+}
